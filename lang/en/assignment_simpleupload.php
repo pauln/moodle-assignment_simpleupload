18fedcbab120a656622b8b35cf4ef3cfb8ebd971
Paul Nicholls <paul.nicholls@canterbury.ac.nz>
2011-10-19 10:54:03 +1300

    Add "manage files" string

--- lang/en/assignment_simpleupload.php
+++ lang/en/assignment_simpleupload.php
@@ -4,3 +4,4 @@ $string['typesimpleupload'] = 'Simple Upload (Multiple Files)';
 $string['upload'] = 'Upload this file';
 $string['addafile'] = 'Add a file';
 $string['advanceduploadafile'] = 'Advanced Upload';
+$string['managefiles'] = 'Manage Files';
