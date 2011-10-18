5f75fa063fc2c52ef37ff3241acdbf28b9c5093f
Paul Nicholls <paul.nicholls@canterbury.ac.nz>
2011-10-18 13:33:04 +1300

    Initial commit

---
+++ lang/en/assignment_simpleupload.php
@@ -0,0 +1,6 @@
+<?php
+
+$string['typesimpleupload'] = 'Simple Upload (Multiple Files)';
+$string['upload'] = 'Upload this file';
+$string['addafile'] = 'Add a file';
+$string['advanceduploadafile'] = 'Advanced Upload';
